796697d31f8873c27fc764a81d2c6212e4a84db7
rehamFatima <reham@ebi.ac.uk>
2018-08-13 17:22:03 +0100

    correcting pylint errors

--- atac_seq/tests/test_pipeline_atac_seq.py
+++ atac_seq/tests/test_pipeline_atac_seq.py
@@ -23,7 +23,7 @@ import pytest
 from basic_modules.metadata import Metadata
 from mg_process_fastq.tool.bowtie_aligner import bowtie2AlignerTool
 from mg_process_fastq.tool.bowtie_indexer import bowtieIndexerTool
-from atac_seq.tool.atac_seq import atacSeqTool
+from atac_seq.process_atac_seq import atacSeq
 
 
 @pytest.mark.atacseq
@@ -50,77 +50,6 @@ def test_atac_seq():
             "gapped_peak": resource_path + "atacseq.Human.ERR1659027_peaks.gappedPeak"
     }
 
-    atac_handle = atacSeqTool()
+    atac_handle = atacSeq()
     atac_handle.run(files, metadata, files_out)
 
-    # Add tests for all files created
-    """for f_out in tg_files:
-        print("TRIM GALORE RESULTS FILE:", f_out)
-        assert tg_files[f_out] == files_out[f_out]
-        assert f_out in tg_meta
-        assert os.path.isfile(tg_files[f_out]) is True
-        assert os.path.getsize(tg_files[f_out]) > 0
-    """
-
-"""
-@pytest.mark.trimgalore
-@pytest.mark.pipeline
-def test_trim_galore_pipeline_02():
-    /""/"
-    Test case to ensure that the trimgalore pipeline code works
-    for paired end data.
-
-    Running the pipeline with the test data from the command line:
-
-    .. code-block:: none
-
-       runcompss                                                         \\
-          --lang=python                                                  \\
-          --library_path=${HOME}/bin                                     \\
-          --pythonpath=/<pyenv_virtenv_dir>/lib/python2.7/site-packages/ \\
-          --log_level=debug                                              \\
-          process_trim_galore.py                                         \\
-             --taxon_id 9606                                             \\
-             --fastq1 /<dataset_dir>/bsSeeker.Mouse.SRR892982_1.fastq.gz \\
-             --fastq2 /<dataset_dir>/bsSeeker.Mouse.SRR892982_2.fastq.gz
-
-#    "/""
-    resource_path = os.path.join(os.path.dirname(__file__), "data/")
-
-    files = {
-        'fastq1': resource_path + 'bsSeeker.Mouse.SRR892982_1.fastq.gz',
-        'fastq2': resource_path + 'bsSeeker.Mouse.SRR892982_2.fastq.gz'
-    }
-
-    metadata = {
-        "fastq1": Metadata(
-            "data_wgbs", "fastq", files['fastq1'], None,
-        ),
-
-        "fastq2": Metadata(
-            "data_wgbs", "fastq", files['fastq2'], None,
-        )
-    }
-
-    files_out = {
-        "fastq1_trimmed": 'tests/data/bsSeeker.Mouse.SRR892982_1.trimmed.fastq.gz',
-        "fastq2_trimmed": 'tests/data/bsSeeker.Mouse.SRR892982_2.trimmed.fastq.gz',
-        "fastq1_report": 'tests/data/bsSeeker.Mouse.SRR892982_1.trimmed.report.txt',
-        "fastq2_report": 'tests/data/bsSeeker.Mouse.SRR892982_2.trimmed.report.txt'
-    }
-
-    tg_handle = process_trim_galore()
-    tg_files, tg_meta = tg_handle.run(files, metadata, files_out)
-
-    # Checks that the returned files matches the expected set of results
-    assert len(tg_files) == 2
-    print (tg_meta)
-
-    # Add tests for all files created
-    for f_out in tg_files:
-        print("TRIM GALORE RESULTS FILE:", f_out)
-        assert tg_files[f_out] == files_out[f_out]
-        assert f_out in tg_meta
-        assert os.path.isfile(tg_files[f_out]) is True
-        assert os.path.getsize(tg_files[f_out]) > 0
-    """
