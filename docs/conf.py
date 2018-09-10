3f7aeb4bf25acc52521d39d39307fbef7589cbd4
rehamFatima <reham@ebi.ac.uk>
2018-09-10 15:56:28 +0100

    removing space between ,

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['mg_process_fastq' , 'alignerUtils']
+MOCK_MODULES = ['mg_process_fastq', 'alignerUtils']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 import atac_seq.tool
