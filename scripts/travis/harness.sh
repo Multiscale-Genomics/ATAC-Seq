8697b7752910c5f9057964c48f55c745e9745565
rehamFatima <reham@ebi.ac.uk>
2018-09-14 15:44:15 +0100

    Update harness.sh

--- scripts/travis/harness.sh
+++ scripts/travis/harness.sh
@@ -22,11 +22,11 @@ pv=$(python -c 'import platform; print(platform.python_version())')
 echo "File directory : "
 pwd
 pytest atac_seq/tests/test_pipeline_atac_seq.py
+ls atac_seq/tests/data/
 tc=$?
 rc=$(($rc + $tc))
 bash tidy_data.sh
 echo "Test running"
-ls atac_seq/tests/data/
 #$fi
 
 
